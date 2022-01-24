3b9ee1e2db9e6e3116844fba5efcc64a5aa1f492
Juha Ylikoski <juha.ylikoski14@outlook.com>
2022-01-24 03:00:57 +0200

    Changed role to be dropdown

--- Valtakunta_app.py
+++ Valtakunta_app.py
@@ -1,5 +1,7 @@
 import kivy
-from kivy.app import App
+from kivymd.app import MDApp
+from kivy.metrics import dp
+
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
@@ -7,6 +9,9 @@ from kivy.uix.popup import Popup
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
+from kivymd.uix.menu import MDDropdownMenu
+from kivymd.uix.list import OneLineIconListItem
+from kivy.properties import StringProperty
 
 import enum
 from typing import List
@@ -15,21 +20,25 @@ from dataclasses import dataclass
 
 roles = ["kansalainen", "kurtesaani", "orja", "aatelinen", "hallitsija"]
 
+class InValuesMeta(enum.EnumMeta): 
+    def __contains__(cls, item): 
+        return item in [v.value for v in cls.__members__.values()] 
 
-class Role(str, enum.Enum):
+
+class Roles(str, enum.Enum, metaclass=InValuesMeta):
     KANSALAINEN = "kansalainen"
     KURTESAANI = "kurtesaani"
     ORJA = "orja"
     AATELINEN = "aatelinen"
     HALLITSIJA = "hallitsija"
-
+    
 @dataclass
 class Player:
     name: str
-    role: Role
+    role: Roles
 
 
-class ValtakuntaApp(App):
+class ValtakuntaApp(MDApp):
     players: List[Player]
     def __init__(self):
         super().__init__()
@@ -39,27 +48,54 @@ class ValtakuntaApp(App):
         return sm
 
 valtakunta = ValtakuntaApp()
+
+class RolesIconListItem(OneLineIconListItem):
+    """Roles icon list
+    """
+    icon = StringProperty()
 class ConfigWindow(Screen):
+    
+
     player_name = ObjectProperty(None)
     role = ObjectProperty(None)
 
-    _players = []
-
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        menu_items = [
+            {
+                "viewclass": "RolesIconListItem",
+                "icon": "git",
+                "text": f"{role}",
+                "height": dp(56),
+                "on_release": lambda x=f"{role}": self.set_item(x),
+            } for role in sorted(Roles)
+        ]
+        self.menu = MDDropdownMenu(
+            caller=self.ids.role,
+            items=menu_items,
+            position="bottom",
+            width_mult=4,
+        )
+        self.menu.bind()
+    
     def submitbtn(self):
         if self.player_name.text != "" and self.role.text != "":
-            if self.role.text.lower() not in roles:
-                content=Button(text="The inputted role is invalid try: \nkansalainen, kurtesaani, orja, aatelinen, hallitsija")
+            if self.ids.role.text.lower() not in Roles:
+                content=Button(text="The inputted role is invalid try: \n" + ", ".join(role for role in Roles))
                 pop = Popup(title='Invalid Role',content=content, auto_dismiss=False)
                 content.bind(on_press=pop.dismiss)
                 pop.open()
 
             else:
-                self._players.append((self.player_name.text, self.role.text))
-                valtakunta.get_running_app().players.append(Player(self.player_name.text, Role(self.role.text)))
+                valtakunta.get_running_app().players.append(Player(self.player_name.text, Roles(self.ids.role.text)))
                 self.reset()
+    
+    def set_item(self, text_item):
+        self.ids.role.text = text_item
+        self.menu.dismiss()
 
     def start_game(self):
-        if len(self._players) == 0:
+        if len(valtakunta.get_running_app().players) == 0:
             content=Button(text="No players inputted")
             pop = Popup(title='No players',content=content, auto_dismiss=False)
             content.bind(on_press=pop.dismiss)
@@ -71,8 +107,7 @@ class ConfigWindow(Screen):
 
     def reset(self):
         self.player_name.text = ""
-        self.role.text = ""
-
+        self.ids.role.text = "Player role"
 class MainWindow(Screen):
     pass
 
